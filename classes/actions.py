e4775b26f698752105f63ce117a9adaa25e1800f
Daniel dos Santos França <danieldrafts@gmail.com>
2019-08-28 19:28:33 -0300

    novo comando atualizar

--- classes/actions.py
+++ classes/actions.py
@@ -6,42 +6,44 @@ from bs4 import BeautifulSoup
 
 class Actions():
 
+    def __init__(self, assistent):
+        self.assistent = assistent
     def verify_command(self, assistent, command):
         if (command in ['vou te ensinar algo novo']):
-            assistent.play_sound('denied.mp3')
-            print('- '+assistent.name+': Você ainda não me programou para isto!')
-            assistent.anwser('Você ainda não me programou para isto!')
+            self.assistent.play_sound('denied.mp3')
+            print('- '+self.assistent.name+': Você ainda não me programou para isto!')
+            self.assistent.anwser('Você ainda não me programou para isto!')
 
         elif command in ['vá dormir', 'vai dormir', 'fechar']:
-            assistent.play_sound('accepted.mp3')
-            assistent.anwser('Ok, até mais!')
+            self.assistent.play_sound('accepted.mp3')
+            self.assistent.anwser('Ok, até mais!')
             exit()
             return True
 
         elif command =='pode deixar' or command == 'cancelar':
-            assistent.anwser('Como quiser!')
+            self.assistent.anwser('Como quiser!')
             return True
 			
         elif 'que dia é hoje' in command:
             today = datetime.now()
             today_date = today.strftime('%d/%m/%Y')
-            assistent.anwser('Hoje é dia: '+today_date)
+            self.assistent.anwser('Hoje é dia: '+today_date)
             return True
 
         elif 'quantas horas' in command or 'que horas são' in command:
             today = datetime.now()
             today_date = today.strftime('%H:%M')
-            assistent.anwser('São: '+today_date)
+            self.assistent.anwser('São: '+today_date)
             return True
 
         elif command =='repita' or command == 'repita o que eu disser':
-            assistent.play_sound('accepted.mp3')
-            assistent.repeat()
+            self.assistent.play_sound('accepted.mp3')
+            self.repeat()
             return True
 
         elif 'reiniciar sistema' in command:
-            os.system('sudo reboot')
             assistent.anwser('Reiniciando!')
+            os.system('sudo reboot')
             return True
 
         elif command in ['quais são as últimas notícias', 'me dê as últimas notícias', 'me atualize das noticias', 'últimas notícias']:
@@ -55,8 +57,24 @@ class Actions():
         elif 'onde fica' in command or 'pesquise' in command:
             command = command.replace(assistent.hotword, "")
             command = command.replace(" ", "+")
-            assistent.google_search(command)
+            self.google_search(command)
+            return True
+        
+        elif 'atualizar' in command or 'atualize' in command:
+            os.system('https://danieldrafts:dantesm21@github.com/Danieldrafts/assistent-standard.git')
             return True
             
         else:
             return False
+    
+    def repeat(self):
+        self.assistent.anwser('Pode falar')
+        youSaid = self.assistent.voice_capture(message = "Escutando", phrase_time_limit = self.phrase_time_limit)
+        self.assistent.writeMessage('Acho que você disse: "'+youSaid+'"')
+        self.assistent.anwser('Acho que você disse: "'+youSaid)
+
+    def google_search(self, keywords):
+        search = requests.get("https://www.google.com/search?q={}&cr=brazil%20&lr=pt_br".format(keywords))
+        search = BeautifulSoup(search.text, 'html.parser')
+        for result in search.findAll("div", {"class": "g"})[:5]:
+            self.assistent.anwser(result)
