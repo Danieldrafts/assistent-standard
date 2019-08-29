05a140f9afe2a0c334398d367c0d008fe99e92d4
Daniel dos Santos França <danieldrafts@gmail.com>
2019-08-28 21:04:32 -0300

    alterando ordem de validação para encaixar na frase

--- classes/actions.py
+++ classes/actions.py
@@ -18,7 +18,7 @@ class Actions():
             print('- '+self.assistent.name+': Você ainda não me programou para isto!')
             self.assistent.anwser('Você ainda não me programou para isto!')
 
-        elif command in ['vá dormir', 'vai dormir', 'fechar assistente']:
+        elif ['vá dormir', 'vai dormir', 'fechar assistente'] in command:
             self.assistent.play_sound('accepted.mp3')
             self.assistent.anwser('Ok, até mais!')
             exit()
